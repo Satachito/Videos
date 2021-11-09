296c21f5fd53f916dc436739c720266cfe23ad86
sat <satoru.ogura@me.com>
2021-11-09 19:12:06 +0900

    Initial Commit

---
+++ Videos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Videos
+//
+//  Created by 小倉哲 on 2021/11/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Videos/VideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideosApp.swift
+//  Videos
+//
+//  Created by 小倉哲 on 2021/11/09.
+//
+
+import SwiftUI
+
+@main
+struct VideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
